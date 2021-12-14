aaa0170250d16029483a94283fbe865879b82211
AntonyMorgante <antonymorgante1@gmail.com>
2021-12-14 15:21:56 +0100

    html and css no script

---
+++ myscript.js
@@ -0,0 +1,6 @@
+const items=[img/01.jpg, img/02.jpg, img/03.jpg, img/04.jpg, img/05.jpg];
+const title=["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
+const text=["Lorem ipsum", "Lorem ipsum", "Lorem ipsum", "Lorem ipsum", "Lorem ipsum"];
+
+const msCurrent=0;
+
